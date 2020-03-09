02cc2ddebd5894729a82dadb01df553471fc7600
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-03-09 17:36:30 -0400

    Created and styled the starting page of the quiz

--- Application/script.js
+++ Application/script.js
@@ -1,10 +1,14 @@
-var timer = document.getElementById('timer');
-var time = document.getElementById('time-left');
+// This is the code for the Code Quiz application.
+// ==============================================
+
+// This bit of code creates and updates the timer at the top-right of the screen.
+var timer = document.getElementById("timer");
+var time = document.getElementById("time-left");
 var secs = 60;
 var gameTime = setInterval(() => {
-    time.textContent = secs;
-    secs--;
-    if (secs == 0) {
-        clearInterval(gameTime);
-    }
-}, 1000)
+  time.textContent = secs;
+  secs--;
+  if (secs == -1) {
+    clearInterval(gameTime);
+  }
+}, 1000);
