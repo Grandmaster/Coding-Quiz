4f4b670684b74d2f106d627d183fef744807c6e9
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-03-09 17:54:00 -0400

    Created quiz questions and options, and put them into arrays

--- Application/script.js
+++ Application/script.js
@@ -12,3 +12,24 @@ var gameTime = setInterval(() => {
     clearInterval(gameTime);
   }
 }, 1000);
+
+// This bit of code creates the questions and options of the quiz. It creates the quiz content.
+var questions = [
+    'What does API stand for?',
+    'What event fires when a user presses a key?',
+    'How would you detect user interactions on your website?'
+]
+var options = [
+    {1: 'Astute Planning Interface',
+    2: 'Arachnid Person Interwebs',
+    3: 'Application Programming Interface',
+    4: 'Augmented Powers of the Illuminati'},
+    {1: 'keyup',
+    2: 'keylog',
+    3: 'keypress',
+    4: 'keydown'},
+    {1: 'Event listeners',
+    2: 'System hacking',
+    3: 'Keylogging',
+    4: 'Screen Capture'}
+]
