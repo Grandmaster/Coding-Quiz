db6bb88826c04677498416dc6c6047ab59e3f22f
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-03-09 22:38:28 -0400

    Created develop branch

--- Application/script.js
+++ Application/script.js
@@ -15,21 +15,22 @@ var gameTime = setInterval(() => {
 
 // This bit of code creates the questions and options of the quiz. It creates the quiz content.
 var questions = [
-    'What does API stand for?',
-    'What event fires when a user presses a key?',
-    'How would you detect user interactions on your website?'
-]
+  "What does API stand for?",
+  "What event fires when a user presses a key?",
+  "How would you detect user interactions on your website?"
+];
 var options = [
-    {1: 'Astute Planning Interface',
-    2: 'Arachnid Person Interwebs',
-    3: 'Application Programming Interface',
-    4: 'Augmented Powers of the Illuminati'},
-    {1: 'keyup',
-    2: 'keylog',
-    3: 'keypress',
-    4: 'keydown'},
-    {1: 'Event listeners',
-    2: 'System hacking',
-    3: 'Keylogging',
-    4: 'Screen Capture'}
-]
+  {
+    1: "Astute Planning Interface",
+    2: "Arachnid Person Interwebs",
+    3: "Application Programming Interface",
+    4: "Augmented Powers of the Illuminati"
+  },
+  { 1: "keyup", 2: "keylog", 3: "keypress", 4: "keydown" },
+  {
+    1: "Event listeners",
+    2: "System hacking",
+    3: "Keylogging",
+    4: "Screen Capture"
+  }
+];
